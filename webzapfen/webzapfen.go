e6c069cbde77bb9e08198c99615a65adbfe649e8
Johann Höchtl <johann.hoechtl@gmail.com>
2012-03-02 10:50:26 +0100

    Don't close the gzip Writer in case of panic, causes multiple writes to http.WriteHeader

--- webzapfen/webzapfen.go
+++ webzapfen/webzapfen.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
-	//	"html/template"
 	"bytes"
+	"compress/gzip"
+	"fmt"
+	"html/template"
 	"io"
 	"net/http"
 	"strings"
@@ -19,7 +20,10 @@ const (
 	redirectparam    = "redirect"
 )
 
-type webhandler func(w io.Writer, req *http.Request, lang string) error
+type webhandler struct {
+	handler  func(w io.Writer, req *http.Request, lang string) error
+	compress bool
+}
 
 func zapfenHandler(w io.Writer, req *http.Request, lang string) (err error) {
 	_, err = fmt.Fprint(w, "In Zapfen")
@@ -30,84 +34,107 @@ func rootHandler(w io.Writer, req *http.Request, lang string) (err error) {
 
 	_, err = fmt.Fprintf(w, "Got language: %s", lang)
 
-	/*tpl, err := template.ParseFiles(language + "." + roottplfilename)
+	tpl, err := template.ParseFiles(lang + "." + roottplfilename)
 	if err != nil {
 		panic(err)
 	}
-
 	err = tpl.Execute(w, nil)
-	*/
-	return
-}
-
-func frHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(w, "Bonjour1")
 	return
 }
 
 // ServeHTTP installs a catch-all error recovery for the specific handler functions
-func (fn webhandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	// error handler 
-	// Recover from panic by setting http error 500 and letting the user know the reason
+// It may gzip-compress the output depending on webhandler.compress 
+func (wh webhandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	// error handler: Recover from panic by setting http error 500 and letting the user know the reason
 	defer func() {
 		if err := recover(); err != nil {
 			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
 		}
 	}()
 
-	// read language cookie. If not set, initialize and set it with the default language,
-	// then redirect to the language subdomain
 	var language string
 	var setcookie bool
 	var mustredirect bool
 
+	// set the language according to the request parameter.
 	if language = req.URL.Query().Get(setlanguageparam); language != "" {
+		// if the language was specified as a paramter, we have to set a cookie	
 		setcookie = true
 	} else {
 		language = defaultlang
 	}
 
+	// read language cookie. If not set, initialize and set it with the language we determined so far,
+	// either the default language or the language given as a request parameter
 	langCookie, err := req.Cookie(cookie_lang)
 	if err == http.ErrNoCookie || setcookie {
 		langCookie = &http.Cookie{Name: cookie_lang, Value: language, Path: "/", Domain: "." + rootdomain}
 		http.SetCookie(w, langCookie)
+		// We set the cookie and redirect to the language subdomain
 		mustredirect = true
 	} else if err != nil {
 		panic(err)
 	}
 
+	// read the language out of the cookie
 	language = langCookie.Value
 
+	// if we have a cookie, but we are not on the correct language-specific subdomain, redirect
 	if strings.Index(req.Host, language) != 0 || mustredirect {
-		var redirect string
+
+		// BEGIN: functionality required? If yes, should redirect relatively or absolutely?
+		var redirect, scheme string
 		if redirect = req.URL.Query().Get(redirectparam); redirect == "" {
 			redirect = req.URL.Path
 		}
+		// END: functionality required? If yes, should redirect relatively or absolutely?
 
-		scheme := req.URL.Scheme
-		if scheme == "" {
+		if req.TLS == nil {
 			scheme = "http://"
+		} else {
+			scheme = "https://"
 		}
-		// functionality required? If yes, should redirect relatively or absolutely?
 		http.Redirect(w, req, scheme+language+"."+rootdomain+applicationport+redirect, http.StatusSeeOther)
 		return
 	}
 
+	// gzip-compression of the output of the given webhandler
+	// The flow is as follows: The webhandler writes into a buffer. If compression is supported
+	// (browser sends Accept-Encoding: gzip), the responsewriter gets pipped through a gzip-Writer
+	// As the webhandler returns, the buffer is written to the outputstream,
+	// which is either the responsehandler, or the gzip-Writer
+	// Normal: bytes.Buffer --> http.ResponseWriter
+	// Compression: bytes.Buffer --> gzip.Writer --> http.ResponseWriter
+	var outputstream io.Writer
+	var gzwriter io.WriteCloser
+	var docompress bool
+	if wh.compress && strings.Contains(req.Header.Get("Accept-Encoding"), "gzip") {
+		docompress = true
+		gzwriter = gzip.NewWriter(w)
+		outputstream = gzwriter
+	} else {
+		outputstream = w
+	}
+
 	buf := new(bytes.Buffer)
-	if err := fn(buf, req, language); err != nil {
-		// might as well panic(err) but we add some more info
-		// we  serialize the error here in the chosen encoding
+	if err := wh.handler(buf, req, language); err != nil {
 		fmt.Fprint(buf, err)
 		w.WriteHeader(http.StatusInternalServerError)
 	}
-	buf.WriteTo(w)
+
+	// Only necessary when doing gzip-compression, otherwise already set to the correct content-type of the actual payload
+	// I we do not set the content type here, the encoding is delivered as content-type application/x-gzip and causes the browser
+	// to download the content instead of displaying 
+	if docompress {
+		defer gzwriter.Close() // Otherwise the content won't get flushed to the output stream
+		w.Header().Set("Content-Encoding", "gzip")
+		w.Header().Set("Content-Type", http.DetectContentType(buf.Bytes()))
+	}
+	buf.WriteTo(outputstream)
 }
 
 func main() {
-
-	http.Handle("/", webhandler(rootHandler))
-	http.Handle("/zapfen/", webhandler(zapfenHandler))
-	http.HandleFunc("fr.webzapfen.hoechtl.at:1112/", frHandler)
+	http.Handle("/", &webhandler{rootHandler, true})
+	http.Handle("/zapfen/", &webhandler{zapfenHandler, true})
 	http.ListenAndServe(applicationport, nil)
-
 }
